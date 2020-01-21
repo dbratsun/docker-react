0f79302be9082836af03ba294a815bfb919d31f5
Dmitry Bratsun <dmitry.bratsun@gmail.com>
2020-01-21 23:42:40 +0300

    changing App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I was changed on the Feature branch!! Edit <code>src/App.js</code> and
-          save to reload.
-        </p>
+        <p>Last changes!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
